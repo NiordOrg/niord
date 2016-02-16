d7e3d2a41d966909852d3e4cca873654a0af6182
peder280370 <peder@carolus.dk>
2016-02-16 14:40:03 +0100

    Start implementing an OSM-based AtoN model

---
+++ niord-model/src/main/java/org/niord/model/vo/aton/AtonBoundsVo.java
@@ -0,0 +1,118 @@
+/* Copyright (c) 2011 Danish Maritime Authority
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.niord.model.vo.aton;
+
+import org.niord.model.IJsonSerializable;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import java.util.Arrays;
+
+/**
+ * An AtoN OSM bounds entity.
+ *
+ * The AtoN model adheres to the OSM seamark specification, please refer to:
+ * http://wiki.openstreetmap.org/wiki/Key:seamark
+ * and sub-pages.
+ * <p>
+ * The model represents a subset of the OSM model, please refer to:
+ * https://github.com/oschrenk/osm/blob/master/osm-io/src/main/resources/OSMSchema.xsd
+ * <p>
+ * Example:
+ * <pre>
+ *   &lt;osm version="0.6" generator="Niord"&gt;
+ *     &lt;node id="672436827" lat="50.8070813" lon="-1.2841124" user="malcolmh" uid="128186" visible="true" version="11"
+ *           changeset="9107813" timestamp="2011-08-23T21:22:36Z"&gt;
+ *         &lt;bounds minlat="34.0662408634219" minlon="-118.736715316772" maxlat="34.0731374116421" maxlon="-118.73122215271" /&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:category" v="north"/&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:colour" v="black;yellow"/&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:colour_pattern" v="horizontal"/&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:shape" v="pillar"/&gt;
+ *         &lt;tag k="seamark:light:character" v="VQ"/&gt;
+ *         &lt;tag k="seamark:light:colour" v="white"/&gt;
+ *         &lt;tag k="seamark:name" v="Calshot"/&gt;
+ *         &lt;tag k="seamark:topmark:colour" v="black"/&gt;
+ *         &lt;tag k="seamark:topmark:shape" v="2 cones up"/&gt;
+ *         &lt;tag k="seamark:type" v="buoy_cardinal"/&gt;
+ *     &lt;/node&gt;
+ *   &lt;/osm&gt;
+ * </pre>
+ */
+public class AtonBoundsVo implements IJsonSerializable {
+
+    double minlat;
+    double minlon;
+    double maxlat;
+    double maxlon;
+
+    /** Computes the bbox for the given list of AtoNs */
+    public static AtonBoundsVo fromAtons(AtonNodeVo[] atons) {
+        if (atons == null) {
+            return null;
+        }
+        AtonBoundsVo bounds = new AtonBoundsVo();
+        bounds.minlat = 90;
+        bounds.minlon = 180;
+        bounds.maxlat = -90;
+        bounds.maxlon = -180;
+        Arrays.stream(atons).forEach(a -> {
+            bounds.minlat = Math.min(bounds.minlat, a.lat);
+            bounds.minlon = Math.min(bounds.minlon, a.lon);
+            bounds.maxlat = Math.max(bounds.maxlat, a.lat);
+            bounds.maxlon = Math.max(bounds.maxlon, a.lon);
+        });
+        return bounds;
+    }
+
+    /*************************/
+    /** Getters and Setters **/
+    /*************************/
+
+    @XmlAttribute
+    public double getMinlat() {
+        return minlat;
+    }
+
+    public void setMinlat(double minlat) {
+        this.minlat = minlat;
+    }
+
+    @XmlAttribute
+    public double getMinlon() {
+        return minlon;
+    }
+
+    public void setMinlon(double minlon) {
+        this.minlon = minlon;
+    }
+
+    @XmlAttribute
+    public double getMaxlat() {
+        return maxlat;
+    }
+
+    public void setMaxlat(double maxlat) {
+        this.maxlat = maxlat;
+    }
+
+    @XmlAttribute
+    public double getMaxlon() {
+        return maxlon;
+    }
+
+    public void setMaxlon(double maxlon) {
+        this.maxlon = maxlon;
+    }
+}
---
+++ niord-model/src/main/java/org/niord/model/vo/aton/AtonNodeVo.java
@@ -0,0 +1,214 @@
+/* Copyright (c) 2011 Danish Maritime Authority
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.niord.model.vo.aton;
+
+import org.apache.commons.lang.StringUtils;
+import org.niord.model.IJsonSerializable;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * An AtoN OSM seamark node.
+ *
+ * The AtoN model adheres to the OSM seamark specification, please refer to:
+ * http://wiki.openstreetmap.org/wiki/Key:seamark
+ * and sub-pages.
+ * <p>
+ * The model represents a subset of the OSM model, please refer to:
+ * https://github.com/oschrenk/osm/blob/master/osm-io/src/main/resources/OSMSchema.xsd
+ * <p>
+ * Example:
+ * <pre>
+ *     &lt;node id="672436827" lat="50.8070813" lon="-1.2841124" user="malcolmh" uid="128186" visible="true" version="11" 
+ *           changeset="9107813" timestamp="2011-08-23T21:22:36Z"&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:category" v="north"/&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:colour" v="black;yellow"/&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:colour_pattern" v="horizontal"/&gt;
+ *         &lt;tag k="seamark:buoy_cardinal:shape" v="pillar"/&gt;
+ *         &lt;tag k="seamark:light:character" v="VQ"/&gt;
+ *         &lt;tag k="seamark:light:colour" v="white"/&gt;
+ *         &lt;tag k="seamark:name" v="Calshot"/&gt;
+ *         &lt;tag k="seamark:topmark:colour" v="black"/&gt;
+ *         &lt;tag k="seamark:topmark:shape" v="2 cones up"/&gt;
+ *         &lt;tag k="seamark:type" v="buoy_cardinal"/&gt;
+ *     &lt;/node&gt;
+ * </pre>
+ */
+@XmlRootElement(name = "node")
+public class AtonNodeVo implements IJsonSerializable {
+
+    long id;
+    double lat;
+    double lon;
+    String user;
+    long uid;
+    boolean visible;
+    int version;
+    int changeset;
+    Date timestamp;
+    private AtonTagVo[] tags;
+
+    /**
+     * Returns if this AtoN node is a valid seamark
+     * @return if this AtoN node is a valid seamark
+     */
+    public boolean validSeamark() {
+        return StringUtils.isNotBlank(user) &&
+                timestamp != null &&
+                tags != null &&
+                Arrays.stream(tags).allMatch(AtonTagVo::validSeamark);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AtonNodeVo that = (AtonNodeVo) o;
+
+        if (id != that.id) return false;
+        if (Double.compare(that.lat, lat) != 0) return false;
+        if (Double.compare(that.lon, lon) != 0) return false;
+        if (uid != that.uid) return false;
+        if (visible != that.visible) return false;
+        if (version != that.version) return false;
+        if (changeset != that.changeset) return false;
+        if (user != null ? !user.equals(that.user) : that.user != null) return false;
+        if (timestamp != null ? !timestamp.equals(that.timestamp) : that.timestamp != null) return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        return Arrays.equals(tags, that.tags);
+
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = (int) (id ^ (id >>> 32));
+        temp = Double.doubleToLongBits(lat);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(lon);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        result = 31 * result + (int) (uid ^ (uid >>> 32));
+        result = 31 * result + (visible ? 1 : 0);
+        result = 31 * result + version;
+        result = 31 * result + changeset;
+        result = 31 * result + (timestamp != null ? timestamp.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(tags);
+        return result;
+    }
+
+    /*************************/
+    /** Getters and Setters **/
+    /*************************/
+
+    @XmlAttribute
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @XmlAttribute
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    @XmlAttribute
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    @XmlAttribute
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @XmlAttribute
+    public long getUid() {
+        return uid;
+    }
+
+    public void setUid(long uid) {
+        this.uid = uid;
+    }
+
+    @XmlAttribute
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    @XmlAttribute
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @XmlAttribute
+    public int getChangeset() {
+        return changeset;
+    }
+
+    public void setChangeset(int changeset) {
+        this.changeset = changeset;
+    }
+
+    @XmlAttribute
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @XmlElement(name = "tag")
+    public AtonTagVo[] getTags() {
+        return tags;
+    }
+
+    public void setTags(AtonTagVo[] tags) {
+        this.tags = tags;
+    }
+}
