64e11fd736201dfaafaa90fb99a680f43bd05bf9
peder280370 <peder@carolus.dk>
2016-08-17 07:53:39 +0200

    Update license to Apache 2.0

--- niord-core/src/main/java/org/niord/core/util/GlobalMercator.java
+++ niord-core/src/main/java/org/niord/core/util/GlobalMercator.java
@@ -1,38 +1,19 @@
-/* Copyright (c) 2011 Danish Maritime Authority
+/*
+ * Copyright 2016 Danish Maritime Authority.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.niord.core.util;
-
-/**
- *    Copyright (C) 2009, 2010
- *    State of California,
- *    Department of Water Resources.
- *    This file is part of DSM2 Grid Map
- *    The DSM2 Grid Map is free software:
- *    you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *    DSM2 Grid Map is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details. [http://www.gnu.org/licenses]
- *
- *    @author Nicky Sandhu
- *
- */
 
 /**
  * TMS Global Mercator Profile ---------------------------
