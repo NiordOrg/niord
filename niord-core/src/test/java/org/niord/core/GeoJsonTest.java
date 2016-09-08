6b9faf9f9d70791f315ffee183a7f70d25fec2a0
peder280370 <peder@carolus.dk>
2016-09-08 07:16:01 +0200

    Improve unit test

--- niord-core/src/test/java/org/niord/core/GeoJsonTest.java
+++ niord-core/src/test/java/org/niord/core/GeoJsonTest.java
@@ -25,12 +25,15 @@ import org.niord.core.geojson.PlainTextConverter;
 import org.niord.model.geojson.FeatureCollectionVo;
 import org.niord.model.geojson.GeoJsonVo;
 import org.niord.model.geojson.GeometryVo;
+import org.niord.model.geojson.MultiPointVo;
+import org.niord.model.geojson.PolygonVo;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 /**
@@ -118,13 +121,24 @@ public class GeoJsonTest {
         PlainTextConverter converter = PlainTextConverter.newInstance(languages);
 
         String text = "\nPolygon, Goat-Ice\n54° 41,000'N - 010° 35,000'E\n54° 35,566'N - 010° 35,010'E\n54° 38,397'N - 010° 25,125'E";
-        System.out.println(converter.fromPlainText(text));
+        FeatureCollectionVo result = converter.fromPlainText(text);
+        System.out.println(result);
+        assertNotNull(result);
+        assertEquals(1, result.getFeatures().length);
+        assertEquals(PolygonVo.class, result.getFeatures()[0].getGeometry().getClass());
+        assertEquals("Goat-Ice", result.getFeatures()[0].getProperties().get("name:da"));
+        assertEquals("Goat-Ice", result.getFeatures()[0].getProperties().get("name:en"));
 
         text = "1) 54° 45,7' N 10° 29,1' E, Ærø S.\n2) 54° 41,2' N 10° 36,9' E, Keldsnor SW.\n3) 54° 38,3' N 10° 40,1' E, Keldsnor S.";
-        System.out.println(converter.fromPlainText(text));
+        result = converter.fromPlainText(text);
+        assertEquals(MultiPointVo.class, result.getFeatures()[0].getGeometry().getClass());
+        System.out.println(result);
 
         text = "Point, da: Ubåd U-9, en: Submarine U-9.\n54° 45,7' N 10° 29,1' E, da: Ærø S. en: Aeroe S.";
-        System.out.println(converter.fromPlainText(text));
+        result = converter.fromPlainText(text);
+        assertEquals("Ærø S.", result.getFeatures()[0].getProperties().get("name:0:da"));
+        assertEquals("Aeroe S.", result.getFeatures()[0].getProperties().get("name:0:en"));
+        System.out.println(result);
     }
 
 
