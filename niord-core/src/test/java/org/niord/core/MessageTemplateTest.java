e203b0f8450b62feacef3e9af76ad98bf10a6f09
peder280370 <peder@carolus.dk>
2017-02-23 10:05:51 +0100

    More Nashorn tests

--- niord-core/src/test/java/org/niord/core/MessageTemplateTest.java
+++ niord-core/src/test/java/org/niord/core/MessageTemplateTest.java
@@ -17,6 +17,7 @@
 package org.niord.core;
 
 
+import jdk.nashorn.api.scripting.JSObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.niord.core.message.vo.SystemMessageVo;
@@ -28,12 +29,19 @@ import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.SimpleBindings;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 /**
  * Various message template tests
  */
 public class MessageTemplateTest {
 
+    /**
+     * Test that we can use Javascript to update message fields via nested property names,
+     * such as "msg.promulgation('twitter').tweet"
+     */
     @Test
     public void testJavaScriptFieldUpdate() throws Exception {
 
@@ -63,4 +71,49 @@ public class MessageTemplateTest {
     }
 
 
+    /**
+     * Test that we can override the Nashorn load function.
+     *
+     * See https://bugs.openjdk.java.net/secure/attachment/54881/LoaderTest.java
+     */
+    @Test
+    public void testJavaScriptLoadFunction() throws Exception {
+
+        ScriptEngine jsEngine = new ScriptEngineManager()
+                .getEngineByName("Nashorn");
+
+        // Get original load function
+        final JSObject origLoadFn = (JSObject)jsEngine.get("load");
+
+        // Get global. Not really necessary as we could use null too, just for completeness.
+        final JSObject thisRef = (JSObject)jsEngine.eval("(function() { return this; })()");
+
+        // Define a new "load" function
+        final Function<Object, Object> newLoadFn = (source) -> {
+            if (source instanceof String) {
+                final String strSource = (String)source;
+                if (strSource.startsWith("myurlscheme:")) {
+                    // handle "myurlscheme:"
+                    return origLoadFn.call(thisRef, createCustomSource(strSource));
+                }
+            }
+            // Fall back to original load for everything else
+            return origLoadFn.call(thisRef, source);
+        };
+
+        // Replace built-in load with our load
+        jsEngine.put("load", newLoadFn);
+        // Load a dynamically generated script
+        jsEngine.eval("load('myurlscheme:boo'); print(doubleUp('again'));");
+    }
+
+    /**
+     * A custom source must define a "name" and a "script" property
+     */
+    public static Object createCustomSource(final String source) {
+        final Map<String, String> sourceMap = new HashMap<>();
+        sourceMap.put("name", source);
+        sourceMap.put("script", "function doubleUp(txt) { return txt + ' and ' + txt; }");
+        return sourceMap;
+    }
 }
