b85da155372c98a5aaf6da121916a22f86233844
peder280370 <peder@carolus.dk>
2017-03-14 14:53:24 +0100

    Issue #73: Added Freemarker directive for formatting dates in the NAVTEX format

---
+++ niord-core/src/main/java/org/niord/core/script/directive/NavtexDateFormatDirective.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.niord.core.script.directive;
+
+import freemarker.core.Environment;
+import freemarker.template.SimpleDate;
+import freemarker.template.TemplateDirectiveBody;
+import freemarker.template.TemplateDirectiveModel;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateModel;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+
+/**
+ * Will format a date according to the NAVTEX standard
+ */
+@SuppressWarnings("unused")
+public class NavtexDateFormatDirective implements TemplateDirectiveModel {
+
+    private static final String PARAM_DATE = "date";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute(Environment env,
+                        Map params,
+                        TemplateModel[] loopVars,
+                        TemplateDirectiveBody body)
+            throws TemplateException, IOException {
+
+        Date date = null;
+        SimpleDate dateParam = (SimpleDate)params.get(PARAM_DATE);
+        if (dateParam != null && dateParam.getAsDate() != null) {
+            date = dateParam.getAsDate();
+        }
+
+        try {
+            SimpleDateFormat navtexUtcDate = new SimpleDateFormat("ddHHmm 'UTC' MMM yy", Locale.US);
+            navtexUtcDate.setTimeZone(TimeZone.getTimeZone("UTC"));
+            String result = navtexUtcDate.format(date);
+            env.getOut().write(result);
+        } catch (Exception e) {
+            // Prefer robustness over correctness
+        }
+    }
+}
