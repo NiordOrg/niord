a9c41a7620cd38508aafd79d182e8a5e6853055a
peder280370 <peder@carolus.dk>
2017-04-26 13:52:27 +0200

    Minor fix

--- niord-web/src/main/webapp/app/sysadmin/sysadmin-domain-ctrl.js
+++ niord-web/src/main/webapp/app/sysadmin/sysadmin-domain-ctrl.js
@@ -91,7 +91,7 @@ angular.module('niord.admin')
                     name: undefined,
                     timeZone: moment.tz.guess(),
                     messageSortOrder: 'AREA ASC',
-                    color: false,
+                    color: undefined,
                     publish: false,
                     atons: false,
                     templates: false
